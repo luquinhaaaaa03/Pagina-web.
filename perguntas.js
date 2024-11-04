25fd1e9222f61965f9e11c8debc6ac1c1caf0dbd
luquinhaaaaa03 <151534633+luquinhaaaaa03@users.noreply.github.com>
2024-11-04 14:38:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro alvares cabrau'
+)
+
+criaCartao(
+    'Geografia',
+    'em qual região fica Grandes rios PR?',
+    'fica no norte do paraná'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom dia em Inglês?',
+    'Bom dia em ingles é HI (Good morning)'
+)
